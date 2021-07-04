d0907300df9fd6e5d8975b70477572faba812062
aci2n <alvarocalace@hotmail.com>
2021-07-04 15:52:02 -0300

    initial commit

---
+++ discord.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+const client = new Discord.Client();
+
+client.on('message', message => {
+  
+});
+
+
+client.login("ODI5NDQxMzE4MjUzMjMyMjM4.YG4LbA.oNd7MeZexZae6rzJS-RsVn1O3Ro");
