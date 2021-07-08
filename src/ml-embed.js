268b63fd19f1039200b30f02c16854608ba453bd
aci2n <alvarocalace@hotmail.com>
2021-07-08 10:11:42 -0300

    ignore empty/null attributes, collapse newlines, format price

--- src/ml-embed.js
+++ src/ml-embed.js
@@ -1,41 +1,53 @@
 const Discord = require('discord.js');
 
-const MAX_FIELDS = 9;
+const MAX_FIELDS = 6;
 const MAX_DESCRIPTION_LENGTH = 300;
 const MAX_FIELD_LENGTH = 30;
 
 function articleEmbed(message, article) {
-    console.log('creating embed', article.item.id);
     const {item, description} = article;
+    console.log('creating embed', item.id);
+
     return new Discord.MessageEmbed()
         .setColor('#ffe600')
         .setTitle(item.title)
         .setURL(item.permalink)
-        .setDescription(truncate(description.plain_text, MAX_DESCRIPTION_LENGTH))
-        .addField('Precio', `${item.currency_id} ${item.price}`, false)
-        .addFields(item.attributes.sort(isGamerSort).slice(0, MAX_FIELDS).map(attribute => mapAttribute(attribute)))
+        .setDescription(collapseNewlines(truncate(description.plain_text, MAX_DESCRIPTION_LENGTH)))
+        .addField('Precio', formatPrice(item.price, item.currency_id), false)
+        .addFields(extractFields(item.attributes))
         .setTimestamp()
         .setThumbnail(item.secure_thumbnail)
         .setFooter(message.author.username, "https://static.mlstatic.com/org-img/homesnw/img/ml-logo.png?v=3.0");
 }
 
-function isGamerSort(a, b) {
-    return a.id === 'IS_GAMER' ? -1 : 0;
-}
+const formatPrice = (function() {
+    const formatters = new Map();
+    formatters.set('ARS', Intl.NumberFormat('es-AR', {style: 'currency', currency: 'ARS'}));
 
-function mapAttribute(attribute) {
-    return {
-        name: attribute.id === 'IS_GAMER' ? "Es ｇａｍｅｒ" : attribute.name,
-        value: attribute.value_name === null ? 'No' : truncate(attribute.value_name, MAX_FIELD_LENGTH),
-        inline: true
+    return (price, currency) => {
+        const formatter = formatters.get(currency);
+        return formatter ? formatter.format(price) : `${currency} ${price}`;
     }
+}());
+
+function extractFields(attributes) {
+    return [...attributes]
+        .sort((a, _) => a.id === 'IS_GAMER' ? -1 : 0)
+        .filter(attr => attr.name && attr.value_name)
+        .slice(0, MAX_FIELDS)
+        .map(attr => ({
+            name: attr.id === 'IS_GAMER' ? "Es ｇａｍｅｒ" : attr.name,
+            value: truncate(attr.value_name, MAX_FIELD_LENGTH),
+            inline: true
+        }));
 }
 
 function truncate(str, len) {
-    if (str.length <= len) {
-        return str;
-    }
-    return str.substring(0, len - 1) + '…';
+    return str.length <= len ? str : str.substring(0, len - 1) + '…';
+}
+
+function collapseNewlines(str) {
+    return str.replace(/\n/g, " ");
 }
 
 exports.articleEmbed = articleEmbed;
