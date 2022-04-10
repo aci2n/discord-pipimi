d9eb16249ed04c9adbbab122e36a73aa3cea1274
aci2n <alvarocalace@hotmail.com>
2022-04-10 20:00:23 -0300

    syntax highlight debug js log

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -22,7 +22,7 @@ const getEvalCommands = () => {
 
         let result;
         try {
-            logger.debug(() => `Evaluating javascript \`\`\`${expression}\`\`\``);
+            logger.debug(() => `Evaluating javascript \`\`\`js\n${expression}\n\`\`\``);
             const start = Date.now();
             result = eval(expression);
             const elapsed = Date.now() - start;
