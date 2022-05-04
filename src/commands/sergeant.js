e60fab6ef94d61c5d1ca4a7445425ffad48ca9e6
aci2n <alvarocalace@hotmail.com>
2022-05-04 14:36:32 -0300

    refactor carcel move command

--- src/commands/sergeant.js
+++ src/commands/sergeant.js
@@ -1,42 +1,40 @@
-import { GuildMember, VoiceState } from "discord.js";
-import { PipimiCommand } from "../framework/command.js";
+import { GuildMember, User, VoiceState } from "discord.js";
+import { PipimiCommand, PipimiContext } from "../framework/command.js";
 
 /**
  * @returns {PipimiCommand[]}
  */
 const getSergeantCommands = () => {
-    const jailChannelId = "885247701019676732";
+    const jailer = createJailer();
+
+    return [
+        PipimiCommand.prefixed(["carcel"], ["Sergeant"], getCarcelCommandHandler(jailer)),
+        new PipimiCommand("sus", getSusCommandHandler(jailer))
+    ]
+};
+
+/**
+ * @param {Jailer} jailer 
+ */
+const getCarcelCommandHandler = jailer => {
     const customMessages = new Map([
-        ["957017010129235989", "adios mosca"],
+        ["262957323994136578", "adios mosca"],
         ["94885721948561408", "<:poque:660633228536971265>"]
     ]);
 
-    return [PipimiCommand.prefixed(["carcel"], ["Sergeant"], async context => {
+    /**
+     * @param {PipimiContext} context 
+     * @returns {Promise<PipimiContext>}
+     */
+    return async context => {
         const { message, logger } = context;
-        const { mentions, client, guild, channel } = message;
+        const { mentions, channel } = message;
+        const movedUsers = await jailer(context, mentions.users.values());
 
-        /** @type {string[]} */
-        const movedIds = [];
+        logger.debug(() => `Moved ${movedUsers.length} users to jail`);
 
-        for (const user of mentions.users.values()) {
-            const member = await new GuildMember(client, { user }, guild).fetch();
-
-            try {
-                logger.trace(() => `About to move user '${member.id}' to jail`);
-                if (await moveToChannel(member.voice, jailChannelId)) {
-                    movedIds.push(member.id);
-                } else {
-                    logger.debug(() => `User '${member.id}' was not moved (not in voice or already in jail?)`);
-                }
-            } catch (e) {
-                logger.error(() => `Failed to move user '${member.id}': ${e}`);
-                await channel.send(`Failed to move user.`);
-                return context;
-            }
-        }
-
-        if (movedIds.length > 0) {
-            const customId = movedIds.find(id => customMessages.has(id));
+        if (movedUsers.length > 0) {
+            const customId = movedUsers.map(user => user.id).find(id => customMessages.has(id));
             if (customId) {
                 logger.trace(() => `Sending custom jail message for user '${customId}': '${customMessages.get(customId)}'`);
             }
@@ -44,12 +42,85 @@ const getSergeantCommands = () => {
         }
 
         return context;
-    })]
+    };
+};
+
+/**
+ * @param {Jailer} jailer
+ */
+const getSusCommandHandler = jailer => {
+    const pattern = /^(sus|amogus|amongus)$/i;
+    const response = "https://seafile.aci2n.tk/f/2c5fdd4cc3274dfbb53b/?raw=1";
+
+    /**
+    * @param {PipimiContext} context 
+    * @returns {Promise<PipimiContext>}
+    */
+    return async context => {
+        const { message, logger } = context;
+        const { content, author, channel } = message;
+
+        if (pattern.test(content)) {
+            logger.debug(() => "Detected amogus", content, author);
+
+            await Promise.all([
+                channel.send(response),
+                jailer(context, [author])
+            ]);
+        }
+
+        return context;
+    };
+};
+
+/**
+ * @callback Jailer
+ * @param {PipimiContext} context
+ * @param {User[]} users
+ * @returns {Promise<User[]>}
+ */
+
+/**
+ * @returns {Jailer}
+ */
+const createJailer = () => {
+    const jailChannelId = "885247701019676732";
+
+    /**
+     * @type {Jailer}
+     */
+    return async (context, users) => {
+        const { message, logger } = context;
+        const { client, guild, channel } = message;
+
+        /** @type {User[]} */
+        const movedUsers = [];
+
+        for (const user of users) {
+            const member = await new GuildMember(client, { user }, guild).fetch();
+
+            try {
+                logger.trace(() => `About to move user '${member.id}' to jail`);
+                if (await moveToChannel(member.voice, jailChannelId)) {
+                    movedUsers.push(user);
+                } else {
+                    logger.debug(() => `User '${member.id}' was not moved (not in voice or already in jail?)`);
+                }
+            } catch (e) {
+                logger.error(() => `Failed to move user '${member.id}': ${e}`);
+                await channel.send(`Failed to move user.`);
+                throw e;
+            }
+        }
+
+        return movedUsers;
+    };
 };
 
 /**
  * @param {VoiceState} voiceState
  * @param {string} channelId
+ * @returns {Promise<boolean>}
  */
 const moveToChannel = async (voiceState, channelId) => {
     if (!voiceState.channelID || voiceState.channelID === channelId) {
